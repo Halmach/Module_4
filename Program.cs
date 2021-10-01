ad26a981b614ef1d64f029006c16b5bfb9fc122d
Khalmatov <large000@yandex.ru>
2021-10-01 23:30:27 +0300

    Статические свойства класса консоль BackgroundColor,ForegroundColor

--- Program.cs
+++ Program.cs
@@ -7,11 +7,32 @@ namespace Module_4
           
         static void Main()
         {
-            var a = 6;
-            var b = 7;
+			Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
 
-            var c = a != b ? a + b : b;
-            Console.WriteLine(c);
+			var color = Console.ReadLine();
+
+			if (color == "red")
+			{
+				Console.BackgroundColor = ConsoleColor.Red;
+				Console.ForegroundColor = ConsoleColor.Black;
+
+				Console.WriteLine("Your color is red!");
+			}
+
+			else if (color == "green")
+			{
+				Console.BackgroundColor = ConsoleColor.Green;
+				Console.ForegroundColor = ConsoleColor.Black;
+
+				Console.WriteLine("Your color is green!");
+			}
+			else
+			{
+				Console.BackgroundColor = ConsoleColor.Cyan;
+				Console.ForegroundColor = ConsoleColor.Black;
+
+				Console.WriteLine("Your color is cyan!");
+			}
 		}
     }
 }
