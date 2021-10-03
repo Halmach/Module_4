accd973c28194e4fb47d2ebeaad16815a41988f8
Khalmatov <large000@yandex.ru>
2021-10-03 18:45:04 +0300

    Задание 4.3.14 Сделайте перебор данного массива массивов по аналогии с массивом строк.

--- Program.cs
+++ Program.cs
@@ -7,13 +7,17 @@ namespace Module_4
           
         static void Main()
         {
-            var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
-            int sum = 0;
-            for(int i = 0; i < arr.Length; i++)
+            int[][] array = new int[3][];
+            array[0] = new int[2] { 1, 2 };
+            array[1] = new int[3] { 1, 2, 3 };
+            array[2] = new int[5] { 1, 2, 3, 4, 5 };
+            foreach(var item in array)
             {
-                sum += arr[i];
+                foreach(var underItem in item)
+                {
+                    Console.Write(underItem + " ");
+                }
             }
-            Console.WriteLine("Сумма элементов: " + sum);
 
         }
     }
