49eff23fb5d7a5bf0d57f91dd19fd73719617233
Khalmatov <large000@yandex.ru>
2021-10-03 17:24:19 +0300

    Задание 4.3.11

--- Program.cs
+++ Program.cs
@@ -7,10 +7,14 @@ namespace Module_4
           
         static void Main()
         {
-            int[,] array = { { 1, 2, 3 }, { 4, 5, 6 } };
-            foreach (var item in array)
+            int[,] array = { { 1, 2, 3 }, { 5, 6, 7 }, { 8, 9, 10 }, { 11, 12, 13 } };
+            for(int i = 0; i < array.GetUpperBound(1) + 1; i++)
             {
-                Console.Write(item + " ");
+                for(int j = 0; j <array.GetUpperBound(0) + 1; j++)
+                {
+                    Console.Write(array[j, i] + " ");
+                }
+                Console.WriteLine();
             }
             //Console.WriteLine("Последняя буква вашего имени: " + name[name.Length - 1]);
 
