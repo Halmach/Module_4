55a802161f5059441a423d2fc8cb46225d3b31a6
Khalmatov <large000@yandex.ru>
2021-10-03 18:40:06 +0300

    Задание 4.3.13.Необходимо найти сумму всех его элементов.

--- Program.cs
+++ Program.cs
@@ -8,21 +8,12 @@ namespace Module_4
         static void Main()
         {
             var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
-            int i = 1,j = 2;
-            while(i < arr.Length)
+            int sum = 0;
+            for(int i = 0; i < arr.Length; i++)
             {
-                if(arr[i-1] <= arr[i]) { i = j; j++;}
-                else
-                {
-                    int t = arr[i];
-                    arr[i] = arr[i - 1];
-                    arr[i - 1] = t;
-                    i--;
-                    if(i == 0) { i = j; j++;}
-                }
+                sum += arr[i];
             }
-            foreach (var arrItem in arr) Console.Write(arrItem + " ");
-            //Console.WriteLine("Последняя буква вашего имени: " + name[name.Length - 1]);
+            Console.WriteLine("Сумма элементов: " + sum);
 
         }
     }
