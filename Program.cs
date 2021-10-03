12579ed338cd791511983b9ec93de83ba4c49135
Khalmatov <large000@yandex.ru>
2021-10-03 16:02:36 +0300

    Задание 4.3.7

--- Program.cs
+++ Program.cs
@@ -8,13 +8,14 @@ namespace Module_4
         static void Main()
         {     
             Console.WriteLine("Введите свое имя");
-            var name = Console.ReadLine();
-            Console.WriteLine("Ваше имя по буквам:");
-            foreach (var character in name)
+            var name = (Console.ReadLine());
+            Console.WriteLine("Ваше имя по буквам в обратном порядке:");
+            var chNum = name.Length;
+            for(int i = chNum - 1; i >= 0; i --)
             {
-                Console.Write(character + " ");
+                Console.Write(name[i] + " ");
             }
-            Console.WriteLine("Последняя буква вашего имени: " + name[name.Length - 1]);
+            //Console.WriteLine("Последняя буква вашего имени: " + name[name.Length - 1]);
 
         }
     }
