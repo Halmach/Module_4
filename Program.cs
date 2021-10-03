c9476adcea80441c6f1177cdca531a16ae1e70f5
Khalmatov <large000@yandex.ru>
2021-10-03 20:35:58 +0300

    Задание 4.3.17 Реализовать сортировку элементов массива внутри одной строки

--- Program.cs
+++ Program.cs
@@ -2,24 +2,41 @@
 
 namespace Module_4
 {
-    class Program
-    {
-          
-        static void Main()
-        {
-            int[,] numbers = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
-            int num = 0;
-            for (int i = 0; i < numbers.GetUpperBound(0) + 1; i++)
-            {
-                for (int j = 0; j < numbers.GetUpperBound(1) + 1; j++)
-                {
-                    num = (numbers[i,j] > 0) ? num + 1 : num;
-                }
-            }  
-            
-            Console.WriteLine("Количество положительных чисел: " + num);
+	class Program
+	{
 
-        }
-    }
+		static void Main()
+		{
+			int[,] numbers = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
+
+			int temp;
+			for (int k = 0; k < numbers.GetUpperBound(0) + 1; k++)
+			{
+				for (int i = 0; i < numbers.GetUpperBound(1) + 1; i++)
+				{
+					for (int j = i + 1; j < numbers.GetUpperBound(1) + 1; j++)
+					{
+						if (numbers[k, i] > numbers[k, j])
+						{
+							temp = numbers[k, i];
+							numbers[k, i] = numbers[k, j];
+							numbers[k, j] = temp;
+						}
+					}
+				}
+
+
+			}
+			for (int k = 0; k < numbers.GetUpperBound(0) + 1; k++)
+			{
+				Console.Write("{");
+				for (int i = 0; i < numbers.GetUpperBound(1) + 1; i++)
+				{
+					Console.Write(numbers[k, i]);
+				}
+				Console.WriteLine("}");
+			}
+		}
+	}
     
 }
