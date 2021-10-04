b36c732a8b690aef4f29c5ed70c025719ab73734
Khalmatov <large000@yandex.ru>
2021-10-04 23:33:13 +0300

    Задание 4.5.1

--- Program.cs
+++ Program.cs
@@ -7,37 +7,15 @@ namespace Module_4
 
 		static void Main()
 		{
-            //      (string myName, byte age, DayOfWeek day, string birthdate) anketa;
-          //  var (myName, age, day, birthdate) = ("Vasya",(byte)30,DayOfWeek.Wednesday,"22.04.1991");
-            (string Name, string Type, double Age, int NameCount) Pet;
+            (string Name, string Surname,
+            string LogIn, int LoginSize,
+            bool HasPet, double Age,
+            string[] FavColors) User;
+
             
-            Console.WriteLine("What is your pet's name?");
-            Pet.Name = Console.ReadLine();
-            Console.WriteLine("Enter your pet's age");
-         
-            bool isAgeCorrenct = double.TryParse(Console.ReadLine(), out Pet.Age);
-            while (!isAgeCorrenct)
-            {
-                Console.WriteLine("Enter your pet's age");
-                isAgeCorrenct = double.TryParse(Console.ReadLine(), out Pet.Age);
-            }
-            Console.WriteLine("What is your pet's type?");
-            Pet.Type = Console.ReadLine();
-            Console.WriteLine("What is your pet's length of name: ");
-            Pet.NameCount = checked(Convert.ToInt32(Console.ReadLine()));
-            Console.WriteLine("Your pet's name is {0}", Pet.Name);
-            Console.WriteLine("Your pet's age is {0}", Pet.Age);
-            Console.WriteLine("Your pet's type is " + Pet.Type);
-            Console.WriteLine("Your pet's length of name is " + Pet.NameCount);
-            Console.ReadKey();
         }
 	}
 
-    enum Semaphore
-    {
-        Red = 100,
-        Yellow = 200,
-        Green = 300
-    }
+
 
 }
